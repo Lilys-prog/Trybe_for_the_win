cccf4955ffbc5856bde7c352993a717fbddeed23
Lily Silva <lilys.prog@gmail.com>
2023-12-23 12:36:01 -0300

    exercício de fixação 1

---
+++ trybe_activities/ssction 3/ex1.js
@@ -0,0 +1,8 @@
+// exercício com foguetinho
+
+const myName = 'Lily';
+const birthCity = 'Castanhal';
+const birthYear = 1984;
+console.log(myName);
+console.log(birthCity);
+console.log(birthYear);
