aa84e742d115ba5246bba67e0d7ea80a25958cff
Lily Silva <lilys.prog@gmail.com>
2023-12-29 09:34:23 -0300

    trabalhando MOD em if-else

---
+++ trybe_activities/ssction 3/prática/ex6.js
@@ -0,0 +1,14 @@
+// utilizar if-else para encontrar um número par
+
+const number1 = 1;
+const number2 = 25;
+const number3 = 3;
+let result;
+
+if (number1 % 2 === 0 || number2 % 2 === 0 || number3 % 2 === 0) {
+  result = true;
+} else {
+  result = false;
+}
+
+console.log(result);
