8dfdbf8b74cd3f01c0a4bd5bd8a4d8413acbd007
Lily Silva <lilys.prog@gmail.com>
2023-12-31 20:06:30 -0300

    transferindo os arquivos pra passta do dia

--- trybe_activities/ssction 3/ex1.js
+++
@@ -1,8 +0,0 @@
-// exercício com foguetinho
-
-const myName = 'Lily';
-const birthCity = 'Castanhal';
-const birthYear = 1984;
-console.log(myName);
-console.log(birthCity);
-console.log(birthYear);
--- trybe_activities/ssction 3/ex2.js
+++
@@ -1,9 +0,0 @@
-// algumas operações aritméticas
-
-const base = 5;
-const height = 8;
-const area = 5 * 8;
-const perimeter = 2 * 5 + 2 * 8;
-
-console.log(area);
-console.log(perimeter);
