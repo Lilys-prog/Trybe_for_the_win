7850b52fbec7907d37f51f361672af854b356171
Lily Silva <lilys.prog@gmail.com>
2024-01-03 18:52:54 -0300

    arrays e estrutura de repetção FOR

--- trybe_activities/ssction 3/day2/aula-ao-vivo/fazendo-amizade-com-arrays.js
+++ trybe_activities/ssction 3/day2/aula-ao-vivo/fazendo-amizade-com-arrays.js
@@ -1,15 +1,31 @@
-const meuArray = [];
-console.log(meuArray); 
-// []
-console.log(Array.isArray(meuArray)); 
-// true
+// const meuArray = [];
+// console.log(meuArray); 
+// // []
+// console.log(Array.isArray(meuArray)); 
+// // true
 
 const names = ['Lily', 'Scott', 'Mike', 'Molly', 'Chandler', 'Carter'];
-console.log(names);
+// console.log(names);
 // [ 'Lily', 'Scott', 'Mike', 'Molly', 'Chandler', 'Carter' ]
 // Mesmo que se vá usar push ou slice no array, ele pode estar definido como constante
-
-console.log(names[0]);
+// console.log(names[0]);
 // Lily
+// console.log(names.length);
 
-console.log(names.length);
+// ===================
+
+// Estrutura de repetição FOR - PARA e
+// Estrutura de repetição WHILE - DO WHILE
+
+
+// dentro do parênteses colocam-se três limites para o parâmetro index
+// inicialização / parada / incremento ou decremento a cada volta
+for (let index = 0; index < names.length; index += 1) {
+  console.log(index, names[index]);
+}
+// 0 Lily
+// 1 Scott
+// 2 Mike
+// 3 Molly
+// 4 Chandler
+// 5 Carter
