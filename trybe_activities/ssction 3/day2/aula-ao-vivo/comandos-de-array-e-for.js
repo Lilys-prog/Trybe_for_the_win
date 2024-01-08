e8399666d60b7a292e514520696a38f9ae3d155b
Lily Silva <lilys.prog@gmail.com>
2024-01-08 17:55:08 -0300

    comandos de array

---
+++ trybe_activities/ssction 3/day2/aula-ao-vivo/comandos-de-array-e-for.js
@@ -0,0 +1,26 @@
+// .push(novo elemento)
+// insere um novo elemento no fim do array
+
+// .pop()
+// remove o último elemento do array
+
+// .shift()
+// remove o primeiro elemento do array (index === 0)
+
+// .unshift(novo elemento)
+// insere um novo elemento no início do array
+
+// .splice(pos, x)
+// remove o(s) elemento(s) do array a partir da posição determinada
+// pos => posição do elemento inicial a ser removido
+// x => quantos elementos devem ser removidos (se colocar 1 ele remove só o elemento que ocupa a posição determinada)
+
+// .includes(item)
+// verifica a existência de um determinado item no array 
+// também é um método muito usado para buscar dentro da string
+// retorna true ou false
+
+// .indexOf(elemento)
+// bussco o elemento informado e retorna o índice do mesmo
+
+
