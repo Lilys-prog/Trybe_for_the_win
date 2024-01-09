9a243c49f0c97d5453bc846df0d4b8d4a866dbc3
Lily Silva <lilys.prog@gmail.com>
2024-01-09 19:19:57 -0300

    função para somar os elementos de um array qualquer

---
+++ trybe_activities/ssction 3/day3/aula-ao-vivo/somatorio.js
@@ -0,0 +1,18 @@
+// criar uma função que receba (parâmetro) um array de inteiros e retorne (return presente) o somatório desses números
+// para criar essa função corretamente, deve-se prestar atenção aos escopos
+
+const sumArray = (numbersArray) => {
+  let sum = 0; // escopo da função: pode ser usada pelo for e pelo return
+
+  for (let index = 0; index < numbersArray.length; index += 1) {
+       sum += numbersArray[index]; // dentro do for pq deve passar por todo o array
+  }
+
+  return sum; // pode retornar por estar no escopo da função
+}
+
+const array1 = [1, 2, 5];
+const array2 = [1, 1, 2, 3];
+
+console.log(sumArray(array1));
+console.log(sumArray(array2));
