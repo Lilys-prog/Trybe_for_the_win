3d0b0d9f67d3585f94a0141e6d2eeadd0f0db380
Lily Silva <lilys.prog@gmail.com>
2024-01-09 16:33:08 -0300

    intro às funções no js

---
+++ trybe_activities/ssction 3/day3/aula-ao-vivo/fazendo-amizade-com-funcoes.js
@@ -0,0 +1,14 @@
+// forma clássica de escrever uma função: palavra reservada + nome da função
+function classic () {
+
+}
+
+// As funções podem ter parâmetros, que vão dentro do parênteses, e devem ser chamadas para serem executadas. Ao chamar, devem-se colocar os parênteses, mesmo que não haja parâmetro, pois eles significam o pedido de execução imediata da função. Caso ela tenha uma condição para ser executada, os parênteses não estarão na chamada, mas ao se cumprir a condição.
+
+// o console.log só imprime seu conteúdo na saída. Para se obter o produto da função, e usar no código, é necessário que ela tenha RETORNO (palavra-chave = return).
+
+// forma arrow function de escrever uma função (está sendo colocada em uma constante, mas ela é executada da mesma forma):
+const arrowFunction = () => {
+
+}
+
