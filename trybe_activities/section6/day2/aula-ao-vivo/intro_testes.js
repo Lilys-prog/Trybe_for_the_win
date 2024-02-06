cd344e8ff0130620ac4b5d7584bb3c27a1960a67
Lily Silva <lilys.prog@gmail.com>
2024-02-06 16:37:40 -0300

    explicação geral sobre testes unitários e TDD + base de dados de HP

---
+++ trybe_activities/section6/day2/aula-ao-vivo/intro_testes.js
@@ -0,0 +1,14 @@
+// os Testes Unitários testam 'uma unidade' de código
+// é um teste barato, fácil de escrever, ler e alterar
+
+// JEST
+// o jest é um framework de testes muito utilizado e simples
+
+// METODOLOGIA TDD
+// Test Driven Development
+// testes ao longo do desenvolvimento - vai codando, vai testando
+  // fase 1: faz um teste que falhe
+  // fase 2: faz o código funcionar (o teste vai passar)
+  // fase 3: refatora o código para otimizar
+// TESTE PRIMEIRO, DESENVOLVA DEPOIS
+// constrói a estrutura de testes, do que se espera que a aplicação faça, e depois vai se escrevendo o código que passe nesses testes, que cumpra os requisitos
