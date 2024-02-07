a001d7ac62bff889b0489ad4f45c1755b337877e
Lily Silva <lilys.prog@gmail.com>
2024-02-07 17:40:33 -0300

    inicia novo exemplo

---
+++ trybe_activities/section6/day2/conteúdo/install_jest.js
@@ -0,0 +1,9 @@
+// para instalar o jest, usa-se o comando 
+// npm install --save-dev jest
+
+// o trecho "--save-dev" garante que o framework só estará na área de desenvolvimento, não será adicionado ao deploy.
+// depois da instalação é importante ir ao package.json e colocar "jest" no parâmetro de testes - se não estiver
+
+// NÃO ESQUECER que ao iniciar um projeto deve-se usar o comando
+                    // npm init -y
+// para criar o arquivo package.json
