de46c71e42dc7b24da858dce39c26665a5ddc1ba
Lily Silva <lilys.prog@gmail.com>
2024-02-08 21:25:20 -0300

    exercício 4

--- trybe_activities/section6/day2/conteúdo/4exercício/techList.js
+++ trybe_activities/section6/day2/conteúdo/4exercício/techList.js
@@ -1,22 +1,19 @@
 const techList = (techArray, name) => {
-
+  if (techArray.length === 0) {
+    return 'Vazio!'
+  }
+  const techObject = [];
+  const techArraySorted = techArray.sort();
+  for (let index = 0; index < techArray.length; index += 1) {
+    techObject.push({
+      tech: techArraySorted[index],
+      name,
+    })
+  }
+  return techObject;
 }
 
-
-
-
-
-
-
-
+console.log(techList([], 'Lily'));
 
 
 module.exports = techList;
-
-
-
-// formato do retorno:
-// {
-//   tech: 'nomeTecnologia',
-//   name: name,
-// }
