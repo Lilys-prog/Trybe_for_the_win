88ac488cca7d3717d9d6afc0fbd6afe2c164aac2
Lily Silva <lilys.prog@gmail.com>
2024-02-10 14:38:25 -0300

    adiciona os exercícios de fixação 1, 2 e 3

---
+++ trybe_activities/section3/day4/CONTEÚDO/objeto-fix.js
@@ -0,0 +1,17 @@
+// exercício 1
+const player = {
+  name: 'Marta',
+  lastName: 'Silva',
+  age: 34,
+  medals: { golden: 2, silver: 3 },
+  bestInTheWorld: [2006, 2007, 2008, 2009, 2010, 2018],
+}
+console.log(player);
+
+// exercício 2
+const message = `A jogadora ${player.name} ${player.lastName} tem ${player.age} anos de idade`;
+console.log(message);
+
+// exercício 3
+const achievements = `A jogadora ${player.name} ${player.lastName} foi eleita a melhor do mundo por ${player.bestInTheWorld.length} vezes`
+console.log(achievements);
