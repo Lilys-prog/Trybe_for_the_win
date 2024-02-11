f75411b3058775ac6a94b39c0f507241deeff516
Lily Silva <lilys.prog@gmail.com>
2024-02-11 20:28:34 -0300

    acessando elementos pelos parentes

---
+++ trybe_activities/section4/day2/CONTEÚDO/1exemplo/script.js
@@ -0,0 +1,20 @@
+// Acesse o elemento where-are-you.
+const where = document.getElementById('where-are-you');
+
+// Acesse parent a partir de where-are-you e adicione uma color a ele.
+const whereParent = document.getElementById('where-are-you').parentNode;
+whereParent.style.backgroundColor = 'green';
+
+
+// Acesse o first-child-of-child e adicione um texto a ele. Você se lembra dos vídeos da aula anterior? Eles ensinam como fazer isso.
+console.log(whereParent.firstChild);
+
+// Acesse o first-child a partir de parent.
+
+// Acesse o first-child a partir de where-are-you.
+
+// Acesse o texto Attention! a partir de where-are-you.
+
+// Acesse o third-child a partir de where-are-you.
+
+// Acesse o third-child a partir de parent.
