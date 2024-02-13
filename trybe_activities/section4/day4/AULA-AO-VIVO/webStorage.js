e87bf1855829add092ce62330d21e409de6ef1e3
Lily Silva <lilys.prog@gmail.com>
2024-02-13 11:02:40 -0300

    gabarito do exercício 1

--- trybe_activities/section4/day4/AULA-AO-VIVO/webStorage.js
+++ trybe_activities/section4/day4/AULA-AO-VIVO/webStorage.js
@@ -1,23 +1,34 @@
 // o local storage se mantém infinitamente ou até que se execute a limpeza
-  // guarda as informações em pares chave-valor
+  // guarda as informações em pares chave-valor (objeto de armazenamento)
   // quando o local storage retornar o dado no get, ele vai retornar em forma de string; deve-se fazer as conversões adequadas para o uso
-  // para guardar e recuperar objetos no local storage, usam-se os métodos (também para arrays e números)
-  // JSON.stringfy(objeto) - para guardar e 
+
+// ao criar um item no local storage, usa-se (e pode colocar numa variável):
+// const object = localStorage.setItem('chave', valor)
+
+  // para guardar e recuperar objetos no local storage, usam-se os métodos (também para arrays e números):
+  // localStorage.setItem('chave', JSON.stringify(object)) - para guardar e 
   // JSON.parse(localStorage.getItem('nome atribuído à CHAVE no local storage')) - para recuperar
 
-// comandos do localStorage
+  // no caso de números pode-se usar o parseInt só na hora de recuperar o item, mas o método JSON.parse também funciona, pois o JSON.parse RECUPERA O TIPO ESPECIFICADO NA ORIGEM
 
-localStorage.setItem();
+  // -----
+
+// alguns comandos do localStorage:
+
+localStorage.setItem('chave', valor);
 // guarda a informação
 
-localStorage.getItem();
+localStorage.getItem('chave');
 // recupera a informação
+
+localStorage.removeItem('chave')
+// remove item especificado
 
 localStorage.clear();
 // zera as informações salvas no local storage
 
-// ==================================================
 
+// -----
 
 
 // o session storage é apagado ao fechar o navegador/aba do navegador
@@ -25,3 +36,4 @@ localStorage.clear();
 
 // o cookie desaparece com a limpeza ou com uma data especificada de expiração
   // guarda as informações em string
+  // são úteis quando se quer salvar os dados no navegador E no servidor, pois o cookie envia para lá
