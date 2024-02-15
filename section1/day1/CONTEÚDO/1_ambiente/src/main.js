ebfa4a752839e5bd2776d0ab28588109e7266385
Lily Silva <lilys.prog@gmail.com>
2024-02-15 14:35:02 -0300

    função para nanoid - falta o bundler

---
+++ section1/day1/CONTEÚDO/1_ambiente/src/main.js
@@ -0,0 +1,9 @@
+import { nanoid } from 'nanoid';
+
+// const passwordContainer = document.getElementById('senha')
+// console.log(passwordContainer);
+
+const passwordGenerator = () => {
+  console.log(nanoid());
+}
+passwordGenerator()
